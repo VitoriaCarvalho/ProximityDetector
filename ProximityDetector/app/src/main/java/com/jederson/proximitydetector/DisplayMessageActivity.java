d77143a6c096d3a31e97d611019a1fade820c478
JedersonLuz <jedersonalpha@gmail.com>
2019-08-25 19:37:34 -0300

    Tela do sensor completa. Lá o usuário pode escolher dentre alguns níveis de sensibilidade para detecção do sendor como também pode ligar e desligar o monitoramento como bem entender. No entanto, ainda não realiza nenhuma comunicação com um servidor externo.

--- ProximityDetector/app/src/main/java/com/jederson/proximitydetector/DisplayMessageActivity.java
+++ ProximityDetector/app/src/main/java/com/jederson/proximitydetector/DisplayMessageActivity.java
@@ -9,6 +9,13 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.CompoundButton;
+import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,7 +23,7 @@ public class DisplayMessageActivity extends AppCompatActivity implements SensorE
 
     private SensorManager mSensorManager;
     private Sensor mProximity;
-    private static final int SENSOR_SENSITIVITY = 4;
+    private static int SENSOR_SENSITIVITY = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,34 +35,65 @@ public class DisplayMessageActivity extends AppCompatActivity implements SensorE
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
         // Capture the layout's TextView and set the string as its text
-        TextView textView = findViewById(R.id.textView);
+        TextView textView = findViewById(R.id.textEmail);
         textView.setText(message);
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        Spinner editSensitivity = findViewById(R.id.editSensitivity);
+        editSensitivity.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
+                String s = String.valueOf(adapterView.getItemAtPosition(pos));
+                SENSOR_SENSITIVITY = Integer.parseInt(s.split(" ")[0]);
+                Log.d("Sensibilidade do Sensor", "Sensibilidade alterada para " + SENSOR_SENSITIVITY);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mSensorManager.registerListener(this, mProximity, SensorManager.SENSOR_DELAY_NORMAL);
+//        mSensorManager.registerListener(this, mProximity, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        mSensorManager.unregisterListener(this);
+//        mSensorManager.unregisterListener(this);
+    }
+
+    public void onOffSensor(View view) {
+        Switch aSwitch = (Switch) findViewById(R.id.switchSensor);
+        if (aSwitch.isChecked()) {
+            aSwitch.setText(R.string.desativar_sensor);
+            mSensorManager.registerListener(this, mProximity, SensorManager.SENSOR_DELAY_NORMAL);
+        } else {
+            aSwitch.setText(R.string.ativar_sensor);
+            mSensorManager.unregisterListener(this);
+        }
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
         if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
+            TextView textView = findViewById(R.id.textSensor);
             if (event.values[0] >= -SENSOR_SENSITIVITY && event.values[0] <= SENSOR_SENSITIVITY) {
-                //near
-                Toast.makeText(getApplicationContext(), "near", Toast.LENGTH_SHORT).show();
+                // near
+//                Toast.makeText(getApplicationContext(), "near", Toast.LENGTH_SHORT).show();
+                textView.setText("Perto");
+                Log.d("Sensibilidade do Sensor", "Perto");
             } else {
-                //far
-                Toast.makeText(getApplicationContext(), "far", Toast.LENGTH_SHORT).show();
+                // far
+//                Toast.makeText(getApplicationContext(), "far", Toast.LENGTH_SHORT).show();
+                textView.setText("Longe");
+                Log.d("Sensibilidade do Sensor", "Longe");
             }
         }
     }

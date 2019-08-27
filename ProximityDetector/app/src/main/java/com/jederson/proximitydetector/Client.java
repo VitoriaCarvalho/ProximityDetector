ec12927190e417f40e0bacc41e727e7ff0c63c65
JedersonLuz <jedersonalpha@gmail.com>
2019-08-27 16:45:04 -0300

    Comunicação entre app e servidor funcionando. Agora o mesmo avisa quando chegaram perto dele e envia suas informações de longitude e latitude.

---
+++ ProximityDetector/app/src/main/java/com/jederson/proximitydetector/Client.java
@@ -0,0 +1,49 @@
+package com.jederson.proximitydetector;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+
+public class Client extends AsyncTask<Void, Void, Void> {
+
+    String host= "192.168.43.68";
+    int port = 12345;
+
+    @Override
+    protected Void doInBackground(Void... arg0) {
+
+        Socket socket = null;
+
+        try {
+            Socket cliente = new Socket(host, port);
+            Log.d("Mensagem", "Conectado com sucesso!");
+            PrintStream mensagem = new PrintStream(cliente.getOutputStream());
+            mensagem.println(String.format("Perto:%s:%s", DisplayMessageActivity.longitude, DisplayMessageActivity.latitude));
+        } catch (Exception e) {
+            Log.d("Mensagem", "Conexão falhou!!");
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+        super.onPostExecute(result);
+    }
+
+}
--- ProximityDetector/app/src/main/java/com/jederson/proximitydetector/DisplayMessageActivity.java
+++ ProximityDetector/app/src/main/java/com/jederson/proximitydetector/DisplayMessageActivity.java
@@ -1,29 +1,54 @@
 package com.jederson.proximitydetector;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
-import android.content.Intent;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.content.Context;
-import android.os.Bundle;
-import android.os.Debug;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.CompoundButton;
-import android.widget.Spinner;
-import android.widget.Switch;
+import android.hardware.SensorEvent;
+import android.hardware.Sensor;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.widget.TextView;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Switch;
 import android.widget.Toast;
 
-public class DisplayMessageActivity extends AppCompatActivity implements SensorEventListener {
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import android.os.Bundle;
+import android.view.View;
+import android.util.Log;
+
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class DisplayMessageActivity extends AppCompatActivity implements SensorEventListener, LocationListener {
 
     private SensorManager mSensorManager;
     private Sensor mProximity;
     private static int SENSOR_SENSITIVITY = 4;
+
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
+    protected static String latitude, longitude;
+    protected boolean gps_enabled, network_enabled;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,20 +66,11 @@ public class DisplayMessageActivity extends AppCompatActivity implements SensorE
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
 
-        Spinner editSensitivity = findViewById(R.id.editSensitivity);
-        editSensitivity.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
-                String s = String.valueOf(adapterView.getItemAtPosition(pos));
-                SENSOR_SENSITIVITY = Integer.parseInt(s.split(" ")[0]);
-                Log.d("Sensibilidade do Sensor", "Sensibilidade alterada para " + SENSOR_SENSITIVITY);
-            }
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
 
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
+        Location location = locationManager.getLastKnownLocation(locationManager.GPS_PROVIDER);
+        onLocationChanged(location);
     }
 
     @Override
@@ -89,9 +105,18 @@ public class DisplayMessageActivity extends AppCompatActivity implements SensorE
 //                Toast.makeText(getApplicationContext(), "near", Toast.LENGTH_SHORT).show();
                 textView.setText("Perto");
                 Log.d("Sensibilidade do Sensor", "Perto");
+//                dispatchTakePictureIntent();
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    // Permission is not granted
+                    Toast.makeText(getApplicationContext(), "Permission is not granted", Toast.LENGTH_LONG).show();
+                } else {
+                    // Permission is granted
+                    Client client = new Client();
+                    client.execute();
+                }
             } else {
                 // far
-//                Toast.makeText(getApplicationContext(), "far", Toast.LENGTH_SHORT).show();
                 textView.setText("Longe");
                 Log.d("Sensibilidade do Sensor", "Longe");
             }
@@ -101,5 +126,38 @@ public class DisplayMessageActivity extends AppCompatActivity implements SensorE
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+//        Toast.makeText(getApplicationContext(), "GPS", Integer.parseInt("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude()));
+        try {
+            if(location!= null){
+//                double latitude = location.getLatitude();
+//                double longitude = location.getLongitude();
+                latitude = String.valueOf(location.getLatitude());
+                longitude = String.valueOf(location.getLongitude());
+                Log.d("GPS", "Latitude:" + latitude + ", Longitude:" + longitude);
+            } else {
+                Log.d("GPS", "Latitude: Null" + ", Longitude: Null");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+        Log.d("Latitude","status");
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+        Log.d("Latitude","enable");
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+        Log.d("Latitude","disable");
     }
 }
